5c560672d8c18ddfe2c5fc730a0cad2c4936aac3
Nicola Battilani <basti35@gmail.com>
2016-08-08 17:39:19 +0200

    Description parametrized (urdf -> xacro). Changed behaviors of buttons. Publishing current velocity.

--- omni_common/src/omni_state.cpp
+++ omni_common/src/omni_state.cpp
@@ -2,6 +2,7 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Wrench.h>
 #include <geometry_msgs/WrenchStamped.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <urdf/model.h>
 #include <sensor_msgs/JointState.h>
 
@@ -46,7 +47,7 @@ struct OmniState {
   int buttons[2];
   int buttons_prev[2];
   bool lock;
-  bool close_gripper;
+  bool apply_force;
   hduVector3Dd lock_pos;
   double units_ratio;
 };
@@ -55,39 +56,37 @@ class PhantomROS
 {
 public:
   ros::NodeHandle nh_;
-  ros::Publisher state_pub_;
-  ros::Publisher pose_pub_;
-  ros::Publisher button_pub_;
-  ros::Publisher joint_pub_;
+  ros::Publisher state_pub_,
+                 pose_pub_,
+                 button_pub_,
+                 joint_pub_,
+                 curr_vel_pub_;
   ros::Subscriber haptic_sub_;
   std::string omni_name_, ref_frame, units;
 
   OmniState *state_;
 
   void init(OmniState *s) {
-    ros::param::param(std::string("~omni_name"), omni_name_, std::string("phantom"));
     ros::param::param(std::string("~reference_frame"), ref_frame, std::string("/base_link"));
     ros::param::param(std::string("~units"), units, std::string("mm"));
     
     //Publish button state on NAME/button
-    std::string button_topic = omni_name_ + "/button";
-    button_pub_ = nh_.advertise<omni_msgs::OmniButtonEvent>(button_topic.c_str(), 100);
+    button_pub_ = nh_.advertise<omni_msgs::OmniButtonEvent>("button", 100);
 
     //Publish on NAME/state
-    std::string state_topic = omni_name_ + "/state";
-    state_pub_ = nh_.advertise<omni_msgs::OmniState>(state_topic.c_str(), 1);
+    state_pub_ = nh_.advertise<omni_msgs::OmniState>("state", 1);
     
     //Subscribe to NAME/force_feedback
-    std::string force_feedback_topic = omni_name_ + "/force_feedback";
-    haptic_sub_ = nh_.subscribe(force_feedback_topic.c_str(), 100, &PhantomROS::force_callback, this);
+    haptic_sub_ = nh_.subscribe("force_feedback", 100, &PhantomROS::force_callback, this);
 
     //Publish on NAME/pose
-    std::string pose_topic = omni_name_ + "/pose";
-    pose_pub_ = nh_.advertise<geometry_msgs::PoseStamped>(pose_topic.c_str(), 1);
+    pose_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("pose", 1);
     
     //Publish on NAME/joint_states
-    std::string joint_topic = omni_name_ + "/joint_states";
-    joint_pub_ = nh_.advertise<sensor_msgs::JointState>(joint_topic.c_str(), 1);
+    joint_pub_ = nh_.advertise<sensor_msgs::JointState>("joint_states", 1);
+
+    //Publish on NAME/curr_vel
+    curr_vel_pub_ = nh_.advertise<geometry_msgs::TwistStamped>("curr_vel", 1);
 
     state_ = s;
     state_->buttons[0] = 0;
@@ -105,7 +104,7 @@ public:
     state_->pos_hist1 = zeros; //3x1 history of position
     state_->pos_hist2 = zeros; //3x1 history of position
     state_->lock = false;
-    state_->close_gripper = false;
+    state_->apply_force = true;
     state_->lock_pos = zeros;
     if (!units.compare("mm"))
       state_->units_ratio = 1.0;
@@ -118,7 +117,7 @@ public:
     else
     {
       state_->units_ratio = 1.0;
-      ROS_WARN("Unknown units [%s] unsing [mm]", units.c_str());
+      ROS_WARN("Unknown units [%s] using [mm]", units.c_str());
       units = "mm";
     }
     ROS_INFO("PHaNTOM position given in [%s], ratio [%.1f]", units.c_str(), state_->units_ratio);
@@ -146,7 +145,7 @@ public:
     omni_msgs::OmniState state_msg;
     // Locked
     state_msg.locked = state_->lock;
-    state_msg.close_gripper = state_->close_gripper;
+    state_msg.close_gripper = state_->apply_force;
     // Position
     state_msg.pose.position.x = state_->position[0];
     state_msg.pose.position.y = state_->position[1];
@@ -188,16 +187,16 @@ public:
     pose_msg.header = state_msg.header;
     pose_msg.header.frame_id = ref_frame;
     pose_msg.pose = state_msg.pose;
-    pose_msg.pose.position.x /= 1000.0;
-    pose_msg.pose.position.y /= 1000.0;
-    pose_msg.pose.position.z /= 1000.0;
+    pose_msg.pose.position.x /= state_->units_ratio;
+    pose_msg.pose.position.y /= state_->units_ratio;
+    pose_msg.pose.position.z /= state_->units_ratio;
     pose_pub_.publish(pose_msg);
 
     //Enter only if at least one button changed state
     if ((state_->buttons[0] != state_->buttons_prev[0]) || (state_->buttons[1] != state_->buttons_prev[1]))
     {
       if (state_->buttons[0] == 1)
-        state_->close_gripper = !(state_->close_gripper);
+        state_->apply_force = !(state_->apply_force);
       if (state_->buttons[1] == 1)
         state_->lock = !(state_->lock);
       omni_msgs::OmniButtonEvent button_event;
@@ -207,44 +206,60 @@ public:
       state_->buttons_prev[1] = state_->buttons[1];
       button_pub_.publish(button_event);
     }
+
+    //Build the velocity msg
+    geometry_msgs::TwistStamped vel_msg;
+    vel_msg.header = state_msg.header;
+    vel_msg.header.frame_id = ref_frame;
+    vel_msg.twist.linear.x = state_->velocity[0] / state_->units_ratio;
+    vel_msg.twist.linear.y = state_->velocity[1] / state_->units_ratio;
+    vel_msg.twist.linear.z = state_->velocity[2] / state_->units_ratio;
+    curr_vel_pub_.publish(vel_msg);
   }
 };
 
 HDCallbackCode omni_state_callback(void *pUserData)
 {
   OmniState *omni_state = (OmniState *) pUserData;
-  if (hdCheckCalibration() == HD_CALIBRATION_NEEDS_UPDATE) {
+  if (hdCheckCalibration() == HD_CALIBRATION_NEEDS_UPDATE)
+  {
     ROS_DEBUG("Updating calibration...");
-      hdUpdateCalibration(calibrationStyle);
-    }
+    hdUpdateCalibration(calibrationStyle);
+  }
   hdBeginFrame(hdGetCurrentDevice());
+
+
   // Get transform and angles
   hduMatrix transform;
   hdGetDoublev(HD_CURRENT_TRANSFORM, transform);
   hdGetDoublev(HD_CURRENT_JOINT_ANGLES, omni_state->joints);
   hduVector3Dd gimbal_angles;
   hdGetDoublev(HD_CURRENT_GIMBAL_ANGLES, gimbal_angles);
-  // Notice that we are inverting the Z-position value and changing Y <---> Z
-  // Position
-  omni_state->position = hduVector3Dd(transform[3][0], -transform[3][2], transform[3][1]);
-  omni_state->position /= omni_state->units_ratio;
-  // Orientation (quaternion)
+
   hduMatrix rotation(transform);
-  rotation.getRotationMatrix(rotation);
-  hduMatrix rotation_offset( 0.0, -1.0, 0.0, 0.0,
-                             1.0,  0.0, 0.0, 0.0,
-                             0.0,  0.0, 1.0, 0.0,
-                             0.0,  0.0, 0.0, 1.0);
-  rotation_offset.getRotationMatrix(rotation_offset);
-  omni_state->rot = hduQuaternion(rotation_offset * rotation);
+
+  omni_state->position = hduVector3Dd(transform[3][0], transform[3][1], transform[3][2]);
+  omni_state->rot = hduQuaternion(rotation);
+
+/*
+  hduVector3Dd velocities;
+  hdGetDoublev(HD_CURRENT_VELOCITY, velocities);
+
+  std::cout <<
+    velocities[0] << ", " << velocities[1] << ", " << velocities[2] << "\r";
+*/
+
   // Velocity estimation
   hduVector3Dd vel_buff(0, 0, 0);
+
+  //2nd order backward dif, Assuming that the rate is 1kHz.
   vel_buff = (omni_state->position * 3 - 4 * omni_state->pos_hist1
-      + omni_state->pos_hist2) / 0.002;  //(units)/s, 2nd order backward dif
+      + omni_state->pos_hist2) / 0.002;  //(units)/s, 
   omni_state->velocity = (.2196 * (vel_buff + omni_state->inp_vel3)
       + .6588 * (omni_state->inp_vel1 + omni_state->inp_vel2)) / 1000.0
       - (-2.7488 * omni_state->out_vel1 + 2.5282 * omni_state->out_vel2
           - 0.7776 * omni_state->out_vel3);  //cutoff freq of 20 Hz
+
   omni_state->pos_hist2 = omni_state->pos_hist1;
   omni_state->pos_hist1 = omni_state->position;
   omni_state->inp_vel3 = omni_state->inp_vel2;
@@ -254,16 +269,22 @@ HDCallbackCode omni_state_callback(void *pUserData)
   omni_state->out_vel2 = omni_state->out_vel1;
   omni_state->out_vel1 = omni_state->velocity;
   
+  
+
   // Set forces if locked
   if (omni_state->lock == true) {
-    omni_state->force = 0.04 * omni_state->units_ratio * (omni_state->lock_pos - omni_state->position)
-        - 0.001 * omni_state->velocity;
+    omni_state->force = 0.04 * (omni_state->lock_pos - omni_state->position) / omni_state->units_ratio
+      - 0.001 * omni_state->velocity;
   }
   hduVector3Dd feedback;
-  // Notice that we are changing Y <---> Z and inverting the Z-force_feedback
-  feedback[0] = omni_state->force[0];
-  feedback[1] = omni_state->force[2];
-  feedback[2] = -omni_state->force[1];
+
+  if(omni_state->apply_force)
+  {
+    feedback[0] = omni_state->force[0];
+    feedback[1] = omni_state->force[1];
+    feedback[2] = omni_state->force[2];
+  }
+  
   hdSetDoublev(HD_CURRENT_FORCE, feedback);
 
   //Get buttons
@@ -348,7 +369,7 @@ void *ros_publish(void *ptr)
 {
   PhantomROS *omni_ros = (PhantomROS *) ptr;
   int publish_rate;
-  ros::param::param(std::string("~publish_rate"), publish_rate, 1000);
+  ros::param::param(std::string("~/publish_rate"), publish_rate, 1000);
   ROS_INFO("Publishing PHaNTOM state at [%d] Hz", publish_rate);
   ros::Rate loop_rate(publish_rate);
   ros::AsyncSpinner spinner(2);
